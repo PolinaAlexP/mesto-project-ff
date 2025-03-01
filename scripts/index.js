88fc92848c78204c4714f6ac555b0d46bd931a34
Polina Posokhina <paulinaposokhina@yandex.ru>
2025-03-01 21:33:12 +0300

    Финальный 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 
-function addCard (cardData, deleteCard) {
+function createCard (cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   
-  cardElement.querySelector('.card__image').src = cardData.link;
-  cardElement.querySelector('.card__image').alt = cardData.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.link;
   cardElement.querySelector('.card__title').textContent = cardData.name;
 
-  const placesList = document.querySelector('.places__list');
-  placesList.append(cardElement);
-
   cardDeleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
 }
 
 
 function deleteCard (evt) {
-    const cardElement = evt.target.closest('.places__item');
-    cardElement.remove();
+  const cardElement = evt.target.closest('.places__item');
+  cardElement.remove();
 }
 
 
 initialCards.forEach(function(cardData) {
-    addCard(cardData, deleteCard);
+  const newCard = createCard(cardData, deleteCard);
+  const placesList = document.querySelector('.places__list');
+  placesList.append(newCard);
 });
 
