944c9332312015d435c37bed68bfdf997405468c
Polina Posokhina <paulinaposokhina@yandex.ru>
2025-03-01 18:21:45 +0300

    Финальный

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function addCard (cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  const placesList = document.querySelector('.places__list');
+  placesList.append(cardElement);
 
-// @todo: Вывести карточки на страницу
+  cardDeleteButton.addEventListener('click', deleteCard);
+}
+
+
+function deleteCard (evt) {
+    const cardElement = evt.target.closest('.places__item');
+    cardElement.remove();
+}
+
+
+initialCards.forEach(function(cardData) {
+    addCard(cardData, deleteCard);
+});
+
